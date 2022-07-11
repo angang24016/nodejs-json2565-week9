a939784808553cd387aa900500b9acd2a049e709
angang24016 <thisakul5200@gmail.com>
2022-07-11 15:51:04 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http')
+const msg = require(`./message.js`)
+
+const host ="localhost"
+const PORT = 5000
+const server = http.createServer((req,res) => {
+    res.writeHead(200, { 'Content-Type':'Text/plain' })
+    res.write('${msg.message} i love wangyibo')
+    res.end()
+})
+
+server.listen(PORT, () => {
+    console.log(`Start server running at http://${host}:${PORT}`)
+})
