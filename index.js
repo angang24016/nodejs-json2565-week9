4e3e1a5529148e563ae4c047aad3923ccc901d61
angang24016 <thisakul5200@gmail.com>
2022-07-11 16:52:42 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const http = require('http')
 const msg = require(`./message.js`)
+const { loadUser } = require('./user_models')
 
 const host ="localhost"
 const PORT = process.env.PORT || 5000
+
 const server = http.createServer((req,res) => {
+    const jsoncontent = JSON.stringify(loadUser())
+  
     res.writeHead(200 ,{ 'Content-Type':'Text/plain' })
-    res.write(`${msg.message} i love wangyibo`)
+    res.write(`${jsoncontent}`)
     res.end()
 })
 
