7ddf43969767ade4be7e92e6ade457f065fc8036
angang24016 <thisakul5200@gmail.com>
2022-07-11 16:23:15 +0700

    up

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const msg = require(`./message.js`)
 const host ="localhost"
 const PORT = process.env.PORT || 5000
 const server = http.createServer((req,res) => {
-    res.writeHead(200, { 'Content-Type':'Text/plain' })
-    res.write('${msg.message} i love wangyibo')
+    res.writeHead(200 ,{ 'Content-Type':'Text/plain' })
+    res.write(`${msg.message} i love wangyibo`)
     res.end()
 })
 
